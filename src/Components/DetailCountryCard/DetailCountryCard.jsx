7296f7780a7b61c7958420568f9093eba3102a97
amidabrian51 <briankaty1@me.com>
2020-04-20 14:49:40 +0100

    Added styling to the DetailCountryCard page

--- src/Components/DetailCountryCard/DetailCountryCard.jsx
+++ src/Components/DetailCountryCard/DetailCountryCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect } from "react";
 import { useState } from "react";
+import './DetailCountryCard.css';
+
 function DetailCountryCard ({ match }) {
     useEffect(() => {
         fetchItem();
@@ -25,12 +27,52 @@ function DetailCountryCard ({ match }) {
     
       return (
         country && (
-          <div>
-            <h1>Name: {country.name}</h1>
-            <p>Native Name: {country.nativeName}</p>
-            <p>Region: {country.region}</p>
-            <p>Languages: {country.languages.map(({ name }) => name).join(", ")}</p>
-           
+          <div className="detailCard">
+            <div className="container">
+
+              <img className="flag" alt="flag" src={country.flag} />
+
+              <div className="countryName">
+
+                <div className="NativeName">
+                  <h1>{country.name}</h1>
+                  <h2>Native Name:</h2>
+                  <p> {country.nativeName}</p>
+                  <br />
+                  <h2>Population:</h2>
+                  <p> {country.population}</p>
+                  <br />
+                  <h2>Region:</h2>
+                  <p> {country.region}</p>
+                  <br />
+                  <h2>Sub Region:</h2>
+                  <p> {country.subregion}</p>
+                  <br />
+                  <h2>Capital:</h2>
+                  <p> {country.capital}</p>
+                  <br />
+                  <div>
+                    <h2>Border Countries:</h2>{country.borders.map(function(border){
+                    return(
+                    <button className="myButton"> {border} </button>
+                    )
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="domain">
+                <h2>Top Level Domain: </h2>
+                <p>{country.topLevelDomain[0]}</p>
+                <br />
+                <h2>Currencies: </h2>
+                <p>{country.currencies.map(({ name }) => name).join(", ")}</p>
+                <br />
+                <h2>Languages: </h2>
+                <p>{country.languages.map(({ name }) => name).join(", ")}</p>
+                <br />
+              </div>
+            </div>
+
           </div>
         )
       );
