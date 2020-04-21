7a789ed9aab2b48828c3903dd092c1c8d02747d4
amidabrian51 <briankaty1@me.com>
2020-04-21 17:04:00 +0100

    Changed end point but getting errors

--- src/Components/Card/CountryCard.jsx
+++ src/Components/Card/CountryCard.jsx
@@ -3,14 +3,13 @@ import './CountryCard.styles.css';
 import {Link} from 'react-router-dom';
 
 export const CountryCard = (props) => (
-    <Link to={`/DetailCard/${props.country.name}`} >
+    <Link to={`/DetailCard/${props.country.alpha3Code}`} >
     <div className="card-container">
     <img alt="flag" src={props.country.flag}width="100%" height="40%"/>
     <h1>{props.country.name}</h1>
     <p>Population: {props.country.population}</p>
     <p>Region: {props.country.region}</p>
     <p>Capital: {props.country.capital}</p>
-    
     </div>
     </Link>
 )
--- src/Components/DetailCountryCard/DetailCountryCard.jsx
+++ src/Components/DetailCountryCard/DetailCountryCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import { useEffect } from "react";
 import { useState } from "react";
+import { NavBarCard } from '../NavBar/NavBarCard';
 import './DetailCountryCard.css';
+import {Link} from 'react-router-dom';
 
 function DetailCountryCard ({ match }) {
     useEffect(() => {
@@ -10,71 +13,85 @@ function DetailCountryCard ({ match }) {
         // eslint-disable-next-line 
       }, []);
     
-      const [country, setCountry] = useState(null);
+      const [country, setCountry] = useState({});
+      const [darkMode, setDarkMode] = useState(false);
+
+      
+
     
       const fetchItem = async () => {
-        try {
-          const fetchCountry = await fetch(
-            `https://restcountries.eu/rest/v2/name/${match.params.name}`
-          );
-          const country = await fetchCountry.json();
-          setCountry(country[0]);
-          console.log(country[0]);
-        } catch {
-          // leave state alone
-        }
+        const fetchItem = await fetch(
+          `https://restcountries.eu/rest/v2/alpha/${match.params.alpha3Code}`
+        );
+        const country = await fetchItem.json();
+        setCountry(country);
+        console.log(country);
       };
     
       return (
-        country && (
-          <div className="detailCard">
-            <div className="container">
+        
+        
+        <div className={darkMode ? "dark-mode" : "light-mode" }>
+          
+            <NavBarCard handlechange={()=> setDarkMode(prevMode => !prevMode)} moonMode={darkMode ? "moon fas fa-moon" :
+              "moon far fa-moon"}
+              darkMode={darkMode ? "dark-mode" : "light-mode"}/>
+            <div className="detailCard">
+              <Link to="/">
+            <button className="topButton myButton">Back</button>
+            </Link>
+              <div className="container">
 
-              <img className="flag" alt="flag" src={country.flag} />
+                <img className="flag" alt="flag" src={country.flag} />
 
-              <div className="countryName">
+                <div className="countryName">
 
-                <div className="NativeName">
-                  <h1>{country.name}</h1>
-                  <h2>Native Name:</h2>
-                  <p> {country.nativeName}</p>
-                  <br />
-                  <h2>Population:</h2>
-                  <p> {country.population}</p>
-                  <br />
-                  <h2>Region:</h2>
-                  <p> {country.region}</p>
-                  <br />
-                  <h2>Sub Region:</h2>
-                  <p> {country.subregion}</p>
-                  <br />
-                  <h2>Capital:</h2>
-                  <p> {country.capital}</p>
-                  <br />
-                  <div>
-                    <h2>Border Countries:</h2>{country.borders.map(function(border){
-                    return(
-                    <button className="myButton"> {border} </button>
-                    )
-                    })}
+                  <div className="NativeName">
+                    <h1 className="Country">{country.name}</h1>
+                    <h2>Native Name:</h2>
+                    <p> {country.nativeName}</p>
+                    <br />
+                    <h2>Population:</h2>
+                    <p> {country.population}</p>
+                    <br />
+                    <h2>Region:</h2>
+                    <p> {country.region}</p>
+                    <br />
+                    <h2>Sub Region:</h2>
+                    <p> {country.subregion}</p>
+                    <br />
+                    <h2>Capital:</h2>
+                    <p> {country.capital}</p>
+                    <br />
+                    <div>
+                   
+                      <h2>Border Countries:</h2>{country.borders.map(function(border){
+                      return(
+                      <Link to={`/DetailCard/${border}`}>
+                      <button className="myButton"> {border} </button>
+                      </Link>
+                      )
+                      })}
+                    </div>
+                    
                   </div>
                 </div>
+                <div className="domain">
+                  <h2>Top Level Domain: </h2>
+                  <p>{country.topLevelDomain[0]}</p>
+                  <br />
+                  <h2>Currencies: </h2>
+                  <p>{country.currencies.map(({ name }) => name).join(", ")}</p>
+                  <br />
+                  <h2>Languages: </h2>
+                  <p>{country.languages.map(({ name }) => name).join(", ")}</p>
+                  <br />
+                </div>
               </div>
-              <div className="domain">
-                <h2>Top Level Domain: </h2>
-                <p>{country.topLevelDomain[0]}</p>
-                <br />
-                <h2>Currencies: </h2>
-                <p>{country.currencies.map(({ name }) => name).join(", ")}</p>
-                <br />
-                <h2>Languages: </h2>
-                <p>{country.languages.map(({ name }) => name).join(", ")}</p>
-                <br />
-              </div>
-            </div>
 
           </div>
-        )
+        </div>
+        
       );
 }
 
