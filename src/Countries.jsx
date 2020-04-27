fb8820b6b5ad0042dcf409b5761f222ea8e7a692
amidabrian51 <briankaty1@me.com>
2020-04-27 15:31:32 +0100

    Getting long list of regions

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -16,6 +16,7 @@ class Countries extends Component {
             darkMode: false
         }
         this.setDarkMode = this.setDarkMode.bind(this);
+        this.handleRegion = this.handleRegion.bind(this);
     };
 
 
@@ -27,6 +28,10 @@ class Countries extends Component {
     }
     setDarkMode(e){
         this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+    }
+
+    handleRegion(e){
+        this.setState({regionField: e.target.value})
     }
     render() {
         const { countries, searchField, regionField, darkMode } = this.state;
@@ -45,11 +50,22 @@ class Countries extends Component {
                         < SearchBox type="search" placeholder="Search a Country" handlechange={e=> this.setState({
                             searchField: e.target.value })}
                             />
+                            <select onChange={this.handleRegion} value={regionField}>
+                                {countries.map(region => (
+                                <option key={region.alpha2Code}>
+                                    {region.region}
+                                </option>
+                                ))}
+                            </select>
 
-                            < SearchBox type="regions" placeholder="Filter by Regions" handlechange={e=> this.setState({
+                            {/* < SearchBox type="regions" placeholder="Filter by Regions" handlechange={e=> this.setState({
                                 regionField: e.target.value })}
-                                />
+                                /> */}
 
+                            {/* <select onChange={this.handleRegion} value={regionField}>
+                                {new Set(countries.map(country=>country.region)).map(uniqueRegion=> 
+                                <option>{uniqueRegion}</option>)}
+                            </select> */}
                     </div>
                     <CountryList countries={filterCountries} />
 
