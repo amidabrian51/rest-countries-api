ec9ea48c4b002709ae198bc99855bfbcdde2590c
amidabrian51 <briankaty1@me.com>
2020-04-30 12:22:13 +0100

    Added className to image in country card

--- src/Components/Card/CountryCard.jsx
+++ src/Components/Card/CountryCard.jsx
@@ -3,15 +3,12 @@ import './CountryCard.styles.css';
 import {Link} from 'react-router-dom';
 
 
-const imgStyle = {
-    width: "100%",
-    height: "40%"
-}
+
 export const CountryCard = (props) => (
     
     <Link to={`/DetailCard/${props.country.alpha3Code}`} >
     <div className="card-container">
-    <img alt="flag" src={props.country.flag} style={imgStyle}/>
+    <img className="imgStyle" alt="flag" src={props.country.flag}/>
     <h1>{props.country.name}</h1>
     <p>Population: {props.country.population}</p>
     <p>Region: {props.country.region}</p>
